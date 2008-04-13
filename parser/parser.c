676e998ee8092c2ef660b0f346382138becdb1a4
dprevost <dprevost@bdce659d-a14a-0410-ae70-83a44b484ef0>
2008-04-13 04:46:13 +0000

    Preparation work continues.
    git-svn-id: https://errorparser.svn.sourceforge.net/svnroot/errorparser/trunk@7 bdce659d-a14a-0410-ae70-83a44b484ef0

---
+++ parser/parser.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2008 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * MIT License as described by the Open Source Initiative
+ * (http://opensource.org/licenses/mit-license.php) and appearing in
+ * the file COPYING included in the packaging of this library.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+// --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
+
+#include <stdio.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+
+int main( int argc, char * argv[] )
+{
+   return 0;
+}
+
+// --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
+
